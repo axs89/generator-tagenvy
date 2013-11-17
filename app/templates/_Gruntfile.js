fa5530c88a28d7fed33c98a4b8d420c068c7edf2
Jurgen Van de Moere <jurgen@webberid.com>
2013-11-17 10:47:44 +0100

    Added task to concatenate bower components and tagenvy src file

--- app/templates/_Gruntfile.js
+++ app/templates/_Gruntfile.js
@@ -5,16 +5,28 @@ module.exports = function (grunt) {
         // Read tagenvy.json so we can access the config
         tagenvy: grunt.file.readJSON('tagenvy.json'),
 
-        // Task to concatenate bower components
+        // Concatenate bower components
         bower_concat: {
             all: {
-                dest: 'dist/_bower.js'
+                dest: 'src/bower.js'
+            }
+        },
+
+        // Concatenate tagenvy.js and _bower.js
+        concat: {
+            options: {
+                separator: ';'
+            },
+            tagenvy: {
+                src: ['src/bower.js', 'src/tagenvy.js'],
+                dest: 'dist/tagenvy.js'
             }
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-bower-concat');
 
     grunt.registerTask('default', ['build']);
-    grunt.registerTask('build', ['bower_concat']);
+    grunt.registerTask('build', ['bower_concat', 'concat']);
 };

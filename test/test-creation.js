f3cb3ad5fcc44ed1b1482633ee6f7603f25f6874
Jurgen Van de Moere <jurgen@webberid.com>
2013-11-17 06:25:11 +0100

    Added initial unit test for checking if correct files are created when generator is run

--- test/test-creation.js
+++ test/test-creation.js
@@ -1,38 +1,39 @@
 /*global describe, beforeEach, it*/
 'use strict';
 
-var path    = require('path');
+var path = require('path');
 var helpers = require('yeoman-generator').test;
 
 
 describe('tagenvy generator', function () {
-  beforeEach(function (done) {
-    helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
-      if (err) {
-        return done(err);
-      }
+    beforeEach(function (done) {
+        helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+            if (err) {
+                return done(err);
+            }
 
-      this.app = helpers.createGenerator('tagenvy:app', [
-        '../../app'
-      ]);
-      done();
-    }.bind(this));
-  });
-
-  it('creates expected files', function (done) {
-    var expected = [
-      // add files you expect to exist here.
-      '.jshintrc',
-      '.editorconfig'
-    ];
-
-    helpers.mockPrompt(this.app, {
-      'someOption': true
+            this.app = helpers.createGenerator('tagenvy:app', [
+                '../../app'
+            ]);
+            done();
+        }.bind(this));
     });
-    this.app.options['skip-install'] = true;
-    this.app.run({}, function () {
-      helpers.assertFiles(expected);
-      done();
+
+    it('creates expected files in client directory', function (done) {
+        var expected = [
+            // add files you expect inside client directory
+            '.bowerrc',
+            'bower.json',
+            'package.json'
+        ];
+
+        helpers.mockPrompt(this.app, {
+            'name': 'client-one'
+        });
+        this.app.options['skip-install'] = true;
+        this.app.run({}, function () {
+            helpers.assertFiles(expected);
+            done();
+        });
     });
-  });
 });
